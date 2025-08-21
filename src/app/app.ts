5d8d3bb3fe675e150eb6ed1a688718594135926d
Shahnawaz alam <sa467546@gmail.com>
2025-08-22 03:18:12 +0530

    Form initiated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('formDemo');
+  //protected readonly title = signal('formDemo');
+  name = new FormControl('Shah');
+  password = new FormControl('Alma');
+
+  displayValue(){
+    console.log('Name:', this.name.value);
+    console.log('Password', this.password.value);
+  }
 }
